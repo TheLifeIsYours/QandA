e186500a322b30a51519490b5c990e6989c1c083
TheLifeIsYours <TLIYours@gmail.com>
2018-10-09 05:59:47 +0200

    I've done so much that i don't even know what...

--- sockets/base.js
+++ sockets/base.js
@@ -1,12 +1,22 @@
-module.exports = function (io) {
-    io.on('connect', (client) => {
-        client.emit('welcome', {'message':'Welcome to the server...'});
-        client.on('joined', (data) => {
+module.exports = (io) => {
+    io.on('connect', (socket) => {
+        socket.broadcast.to(`${socket.id}`).emit('welcome', {'message':'Welcome to the server...'});
+        
+        socket.on('joined', (data) => {
             console.log(sanitize(data.message));
             console.log(sanitize(data.page));
         });
-    });
 
+        socket.on('newSiteEvent-answer', () => {
+            console.log('siteUpdate Answer Happened');
+            socket.broadcast.emit('siteUpdate', {'message':'A new answer has been posted'});
+        });
+        
+        socket.on('newSiteEvent-question', () => {
+            console.log('siteUpdate Question Happened');
+            socket.broadcast.emit('siteUpdate', {'message':'A new question has been posted'});
+        });
+    });
 }
 
 function sanitize(str) {
