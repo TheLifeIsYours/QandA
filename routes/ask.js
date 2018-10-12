0e1ccfad8fc273dfb405b290515c44e75935e49f
TheLifeIsYours <TLIYours@gmail.com>
2018-10-13 00:00:09 +0200

    A heap of changes to Database tructure
    Changed out simple file read/write to use Cloud Firebase service for storage of data.

--- routes/ask.js
+++ routes/ask.js
@@ -2,6 +2,17 @@ const express = require('express');
 const fs = require('fs');
 const router = express.Router();
 
+/*Database setup
+-------------------------------------------------------*/
+const Firestore = require('@google-cloud/firestore');
+
+const firestore = new Firestore({
+  projectId: 'tliyqa',
+  keyFilename: './bin/firebaseCredentials.json',
+});
+
+const qaCollection = firestore.collection('QandA');
+
 /*Routing
 -------------------------------------------------------*/
 router.get('/', function(req, res, next) {
@@ -11,86 +22,31 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', async (req, res, next) => {
-  res.redirect('/posts');
-
-  let name = req.body.name.length <= 0 ? "Anon" : sanitize(req.body.name);
-  let question = sanitize(req.body.question);
-
-  let jsonData = await getJSON('./database/json/QA.json');
-
-  jsonData.push({'id': newPostID(), 'q':question, 'u':name, 'a':[]});
-
-  postJSON(jsonData, './database/json/QA.json');
+  postQuestion(req.body)
+  .then( res.redirect(`/`) )
+  .catch((err) => { res.redirect(`/error?error=${err}`) });
 });
 
 module.exports = router;
 
-/*Functions and other code
+/*Functions and database connection
 -------------------------------------------------------*/
-const getJSON = async (url) => {
-  let data = await JSON.parse(fs.readFileSync(url));
-  
+
+const postQuestion = (content) => {
+  let name = content.name.length <= 0 ? "Anon" : sanitize(content.name);
+  let question = sanitize(content.question);
+
   return new Promise((resolve, reject) => {
-    isJson(data)
-    .then(resolve(data)) //passes on
-    .catch((error) => {
-      reject(error); //passes on error messasges
-    });
+    qaCollection.add({a:""}).then(ref => {
+      ref.set({id: `${ref.id}`, name: `${name}`, question: `${question}`, answers: []});
+    })
+    .then(resolve())
+      .catch((err) => {
+        console.Error("Error posting answer: ", err);
+        reject(new Error("Error posting answer: ", err));
+      });
   });
-}
-
-const postJSON = (data, url) => {
-  return new Promise((resolve, reject) => {
-    isJson(data)
-    .then(
-      resolve(fs.writeFileSync(url, JSON.stringify(data), 'utf8'))
-    )
-    .catch((error) => {
-      reject(error)
-    });
-  });
-}
-
-const isJson = (data) => {
-  return new Promise((resolve, reject) => {
-    data = typeof data !== 'string' ? JSON.stringify(data) : data;
-
-    try{
-      data = JSON.parse(data);
-    }catch(e){
-      reject(new Error("Failed to parse to JSON format, remember to check if your json format is correct"));
-    }
-  
-    if(typeof data === "object" && data !== null){
-      resolve(true);
-    }
-
-    reject(new Error("Attempted to use invalid JSON format"));
-  });
-}
-
-const newPostID = async () => {
-  let newIDIsUnique = true;
-
-  let data = await getJSON('./database/json/QA.json');
-  let newID = await randString(8);
-
-  for(var post in data)
-  newIDIsUnique = data[post].id == newID ? false : true;
-
-  return newIDIsUnique ? newID : newPostID(); 
-}
-
-const randString = (string_length) => {
-  const chars = "abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let rndString = '';
-
-  for (var i = 0; i < string_length; i++) {
-      var rndI = Math.floor(Math.random() * chars.length);
-      rndString += chars.substring(rndI, rndI + 1);
-  }
-  return rndString;
-}
+};
 
 const sanitize = (str) => {
   return String(str).replace(/&/g, ' ').replace(/</g, ' ').replace(/>/g, ' ').replace(/"/g, ' ').replace(/nbsp;/gi,'');
--- routes/posts.js
+++ routes/posts.js
@@ -1,79 +1,82 @@
+const Firestore = require('@google-cloud/firestore');
 const express = require('express');
-const fs = require('fs');
 const router = express.Router();
+
+const firestore = new Firestore({
+  projectId: 'tliyqa',
+  keyFilename: './bin/firebaseCredentials.json',
+  timestampsInSnapshots: true
+});
+
+const qaCollection = firestore.collection('QandA');
 
 /*Routing
 -------------------------------------------------------*/
 router.get('/', async (req, res, next) => {
   res.render('posts', {
-    title: 'Questions & Answers',
-    posts: await getJSON('./database/json/QA.json')
+    title:  'Questions & Answers',
+    posts:  await getPosts()
+              .then((res) => {
+                return res
+              })
+              .catch((err) => {
+                res.redirect(`/error?error=${err}`)
+              })
   });
 });
 
 router.post('/answer', async (req, res, next) => {
-  res.redirect('/');
-
-  let id = sanitize(req.body.id);
-  let name = req.body.name.length <= 0 ? "Anon" : sanitize(req.body.name);
-  let answer = sanitize(req.body.answer);
-
-  let jsonData = await getJSON('./database/json/QA.json');
-  
-  for(let post in jsonData){
-    if(jsonData[post].id == id){
-      jsonData[post].a.push({'u':name, 'a': answer});
-      postJSON(jsonData, './database/json/QA.json');
-    }
-  }
+  await postAnswer(req.body)
+    .then( res.redirect(`/`) )
+    .catch((err) => { res.redirect(`/error?error=${err}`) });
 });
 
 module.exports = router;
 
+
+
 /*Functions and other code
 -------------------------------------------------------*/
-const getJSON = async (url) => {
-  let data = await JSON.parse(fs.readFileSync(url));
-  
-  return new Promise((resolve, reject) => {
-    isJson(data)
-    .then(resolve(data)) //passes on
-    .catch((error) => {
-      reject(error); //passes on error messasges
-    });
-  });
-}
 
-const postJSON = (data, url) => {
+const getPosts = () => {
   return new Promise((resolve, reject) => {
-    isJson(data)
-    .then(
-      resolve(fs.writeFileSync(url, JSON.stringify(data), 'utf8'))
+    qaCollection.get()
+      .then((snap) => {
+        let posts = [];
+
+        snap.forEach((doc) => {
+          posts.push(doc.data());
+        });
+
+        resolve(posts);
+      })
+      .catch((err) => {
+        console.Error('Error getting questions', err);
+        reject(new Error('Error getting questions', err));
+      });
+  });
+};
+
+const postAnswer = (content) => {
+  let id = sanitize(content.id);
+  let name = content.name.length <= 0 ? "Anon" : sanitize(content.name);
+  let answer = sanitize(content.answer);
+
+  return new Promise((resolve, reject) => {
+    qaCollection.doc(`${id}`).update('answers',
+      Firestore.FieldValue.arrayUnion({
+        name: `${name}`,
+        answer: `${answer}`
+      })
     )
-    .catch((error) => {
-      reject(error)
-    });
+    .then(resolve())
+      .catch((err) => {
+        console.Error("Error posting answer: ", err);
+        reject(new Error("Error posting answer: ", err));
+      });
   });
-}
-
-const isJson = (data) => {
-  return new Promise((resolve, reject) => {
-    data = typeof data !== 'string' ? JSON.stringify(data) : data;
-
-    try{
-      data = JSON.parse(data);
-    }catch(e){
-      reject(new Error("Failed to parse to JSON format, remember to check if your json format is correct"));
-    }
-  
-    if(typeof data === "object" && data !== null){
-      resolve(true);
-    }
-
-    reject(new Error("Attempted to use invalid JSON format"));
-  });
-}
+};
 
 const sanitize = (str) => {
-  return String(str).replace(/&/g, ' ').replace(/</g, ' ').replace(/>/g, ' ').replace(/"/g, ' ').replace(/nbsp;/gi,'');
+  return String(str).replace(/&/g, ' ').replace(/</g, ' ').replace(/>/g, ' ').replace(/"/g, ' ').replace(/nbsp;/gi, '');
 };
