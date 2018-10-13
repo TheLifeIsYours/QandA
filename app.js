f8dd80f06aa724b2fad5da6cd3e44c3198eeeacb
TheLifeIsYours <TLIYours@gmail.com>
2018-10-13 03:45:43 +0200

    changed stylings

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
